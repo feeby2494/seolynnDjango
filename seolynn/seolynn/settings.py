2969026c0bf094af471ce0037afc1dea16874903
Jamie Lynn <toby2494@hotmail.com>
2023-02-09 20:20:06 -0600

    fixing staticfiles in setting.py

--- seolynn/seolynn/settings.py
+++ seolynn/seolynn/settings.py
@@ -149,9 +149,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = Path(BASE_DIR).joinpath('staticfiles')
-STATICFILES_DIRS = (Path(BASE_DIR).joinpath('static'),)
+# STATICFILES_DIRS = (Path(BASE_DIR).joinpath('static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
